f01ae32d939fb76f0786d9dce7cbf936ed90bca8
wangmengchao <158383817@qq.com>
2021-01-07 10:23:21 +0800

    增加路由守卫功能

--- main.js
+++ main.js
@@ -3,13 +3,22 @@ import App from './App'
 //请求器
 import xhr from "./api/xhr.js";
 Vue.prototype.$request = xhr.request;
-
+//路由守卫
+import {router,RouterMount} from './router/router.js'  //路径换成自己的
+Vue.use(router)
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
-app.$mount()
+
+// #ifdef H5
+RouterMount(app, router, '#app')
+// #endif
+
+// #ifndef H5
+app.$mount(); //兼容小程序及app端
+// #endif
--- pages/menu/menu.vue
+++ pages/menu/menu.vue
@@ -1,6 +1,8 @@
 <template>
 	<view>
 		菜单
+		<div @tap="toPage" class="btn"></div>
+
 	</view>
 </template>
 
@@ -8,12 +10,32 @@
 	export default {
 		data() {
 			return {
-				
+
 			};
+		},
+		methods: {
+			toPage: function() {
+				// uni.switchTab({
+				// 	url:'/pages/index/index'
+				// })
+				this.$Router.pushTab({
+					name: 'index',
+					params: {
+						name: '张三',
+						arr: [1, 2, 3, {
+							age: 18
+						}]
+					}
+				})
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	.btn {
+		width: 750rpx;
+		height: 100rpx;
+		border: 2px solid #000;
+	}
 </style>
